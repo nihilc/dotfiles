d3cccadba0b16e19b64b9be6b0d8ea93f47f5196
Christian Cardenas <nihilist.cc@gmail.com>
2025-10-04 09:57:08 -0500

    fix rofi shutdown script

--- config/rofi/script/power-menu.sh
+++ config/rofi/script/power-menu.sh
@@ -3,23 +3,38 @@
 selected_option="$1"
 
 if [ -z "$selected_option" ]; then
-    echo -e "Lock\nLogout\nSuspend\nHibernate\nReboot\nShutdown"
-    exit 0
+  echo -e "Lock\nLogout\nSuspend\nHibernate\nReboot\nShutdown"
+  exit 0
 fi
 
 case "$selected_option" in
-    "Lock")
-        hyprlock > /dev/null 2>&1;;
-    "Logout")
-        hyprctl dispatch exit > /dev/null 2>&1;;
-    "Suspend")
-        hyprlock & systemctl suspend > /dev/null 2>&1;;
-    "Hibernate")
-        hyprlock & systemctl hibernate > /dev/null 2>&1;;
-    "Reboot")
-        systemctl reboot > /dev/null 2>&1;;
-    "Shutdown")
-        systemctl poweroff > /dev/null 2>&1;;
-    *)
-        exit 1 ;;
+"Lock")
+  hyprlock >/dev/null 2>&1
+  ;;
+"Logout")
+  hyprctl dispatch exit >/dev/null 2>&1
+  ;;
+"Suspend")
+  $(
+    hyprlock &
+    sleep 1
+    systemctl suspend
+  ) >/dev/null 2>&1
+  ;;
+"Hibernate")
+  $(
+    hyprlock &
+    sleep 1
+    systemctl hibernate
+  ) >/dev/null 2>&1
+  ;;
+"Reboot")
+  systemctl reboot >/dev/null 2>&1
+  ;;
+"Shutdown")
+  systemctl poweroff >/dev/null 2>&1
+  ;;
+*)
+  exit 1
+  ;;
 esac
