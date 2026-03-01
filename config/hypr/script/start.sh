1d0b1ece98effb4e00cd88a8d6fd6db531699453
Christian Cardenas <nihilist.cc@gmail.com>
2026-03-01 13:37:58 -0500

    remove hyprland rofi and kitty config

--- config/hypr/script/start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-waybar &
-swww-daemon & swww img --transition-type=grow --transition-pos=top ~/Pictures/wallpaper
-swaync &
-nm-applet &
-gammastep &
-wl-paste --type text --watch cliphist store &
-wl-paste --type image --watch cliphist store &
--- config/hypr/script/toggle_waybar.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-PID=$(pgrep waybar)
-
-if [[ -n "$PID" ]]; then
-  pkill waybar
-else
-  waybar
-fi
